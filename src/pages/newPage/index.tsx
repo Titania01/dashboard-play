0fdf3ef73093d895de8831cd623dfd5c0c26e4c0
Titania01 <adegbitebidmus@gmail.com>
2022-05-11 22:38:49 +0100

    feat: new-user page initialization

--- src/pages/newPage/index.tsx
+++ src/pages/newPage/index.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import Navbar from "../../organism/navbar";
+import Sidebar from "../../organism/sidebar";
 import "./newPage.scss";
 
 const NewPage = () => {
-  return <div>NewPage</div>;
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        Test
+      </div>
+    </div>
+  );
 };
 
 export default NewPage;
